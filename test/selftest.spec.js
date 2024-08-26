a3746b3299959db9b9b1c0b82b722410ee917e38
Frank Nocke <git@nocke.de>
2024-08-26 17:25:47 +0000

    format selftest

--- test/selftest.spec.js
+++ test/selftest.spec.js
@@ -1,10 +1,7 @@
 import { assert } from 'chai'
 
-describe('Simple Test', function () {
-  it('should return 2 when adding 1 + 1', function () {
+describe('Simple Test', function() {
+  it('should return 2 when adding 1 + 1', function() {
     assert.equal(1 + 1, 2)
-
-
-
   })
 })
