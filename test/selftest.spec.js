5e89511d7555f761f63e4f6c7f246d9dd4d8eb0b
Frank Nocke <git@nocke.de>
2024-08-26 17:58:32 +0000

    'further' self-test

--- test/selftest.spec.js
+++ test/selftest.spec.js
@@ -4,4 +4,7 @@ describe('Simple Test', function() {
   it('should return 2 when adding 1 + 1', function() {
     assert.equal(1 + 1, 2)
   })
+  it('should return false on unequal objects', function() {
+    assert.notDeepEqual({ banana: 'yes' }, { banana: 'no' })
+  })
 })
